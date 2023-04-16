bc6d98c47afd06f1c1ad35885dc50c5ef1081557
Dilvy Anais <dilvyanais@cy-tech.fr>
2023-04-16 15:13:55 +0200

    fix: syntax

--- BackEnd/model/staff.py
+++ BackEnd/model/staff.py
@@ -7,6 +7,6 @@ Base =declarative_base()
 class Staff(Base):
     __tablename__="staff"
     uid =Column(Integer,primary_key=True, nullable=False)
-    name=  Column(String,nullable=False),
+    name=  Column(String,nullable=False)
     job = Column(String, nullable=False)
     work = relationship('Work', cascade="all,delete",backref="Staff")
