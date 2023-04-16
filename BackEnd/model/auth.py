3ddc1bf5aa4756a231787729ea17126a72320e09
Dilvy Anais <dilvyanais@cy-tech.fr>
2023-04-16 14:24:03 +0200

    add: auth model

---
+++ BackEnd/model/auth.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Auth(Base):
+    __tablename__="auth"
+    uid=Column(Integer, primary_key=True, nullable=False)
+    email= Column(String(100), unique=True, nullable=False)
+    password=Column(String,nullable=False)
+    user=relationship("User", cascade="all,delete", backref="Auth")
--- BackEnd/model/user.py
+++ BackEnd/model/user.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Column, String,Integer
+from sqlalchemy import Column, String,Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from model.auth import Auth
 
 Base = declarative_base() 
 
 class  User(Base):
     __tablename__="wow_user"
-    uid= Column(Integer,primary_key=True, nullable=False)
+    uid= Column(Integer,ForeignKey(Auth.uid),primary_key=True, nullable=False)
     first_name = Column(String(100),nullable=False)
     last_name = Column(String(100), nullable=False)
     age = Column(Integer,nullable=False)
