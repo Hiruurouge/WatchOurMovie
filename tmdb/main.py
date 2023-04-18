0088c6d396dc2092b694710567b710f405f20ca6
Kiefferflo <kiefferflo@cy-tech.fr>
2023-04-18 23:28:28 +0200

    feature: add db link

--- tmdb/main.py
+++ tmdb/main.py
@@ -1,12 +1,27 @@
-from fastapi import FastAPI
-from sqlalchemy.orm import Session
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, Depends
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
 
 from .tmdb import get_and_put_movie_and_all
 
+load_dotenv()
+
+MYSQL_DATABASE_URL= os.getenv('MYSQL_DATABASE_URL')
+
+engine = create_engine(MYSQL_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/tmdb/movie")
-async def put_tmdb_movie_cast_prod_in_db(title: str):
-    # TODO c'est quoi cette Session de ses morts ?
-    db = Session()
+async def put_tmdb_movie_cast_prod_in_db(title: str, db: Session=Depends(get_db),):
     get_and_put_movie_and_all(db,title)
