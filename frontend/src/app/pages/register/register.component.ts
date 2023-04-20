9e98727847a32c20702fe0190488067e5cb72976
Hiruurouge <Hiruurouge@gmail.com>
2023-04-20 21:54:34 +0200

    Revert "feat_register"
    This reverts commit d7cad24769269a8ab3a01f52e4b3a7cde24a391a.

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import * as bcrypt from 'bcryptjs';
 
 @Component({
   selector: 'app-register',
@@ -8,43 +6,5 @@ import * as bcrypt from 'bcryptjs';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-  email: string='';
-  password: string='';
-  firstName: string='';
-  lastName: string='';
-  genre: string='';
-  age: number=0;
 
-  constructor(private http: HttpClient) { }
-
-  onSubmit() {
-    //const salt = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(this.password);
-
-    const formData = {
-      auth: {
-        email: this.email,
-        password: hashedPassword
-      },
-      user: {
-        uid:0,
-        first_name: this.firstName,
-        last_name: this.lastName,
-        genre: this.genre,
-        age: this.age
-      }
-    };
-
-    console.log(formData)
-
-    this.http.post('http://localhost:3212/api/auth/signup', formData)
-      .subscribe(
-        (response: any) => {
-          console.log(response);
-        },
-        (error) => {
-          console.error(error);
-        }
-      );
-  }
 }
