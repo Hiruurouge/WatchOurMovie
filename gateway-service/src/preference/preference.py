61c41f23340928317f192327fd83bcd9cb58ddcf
Dilvy Anais <dilvyanais@cy-tech.fr>
2023-04-24 15:44:05 +0200

    fix: gateway name function

--- gateway-service/src/preference/preference.py
+++ gateway-service/src/preference/preference.py
@@ -39,40 +39,40 @@ def delete_genre_preference(genre: GenreBase,token: Annotated[TokenData,Depends(
     return result.json()
 
 @router.get("/production")
-def get_genre_preference(token: Annotated[TokenData,Depends(get_current_user)]):
+def get_production_preference(token: Annotated[TokenData,Depends(get_current_user)]):
     db_preferences = requests.get(f"{PRODUCTION_URL}/like/all", params={"uid":token.uid})
     db_preferences.raise_for_status()
     return db_preferences.json()
 
 @router.post("/production")
-def create_genre_preference(productions: List[ProductionBase], token: Annotated[TokenData,Depends(get_current_user)]):
+def create_production_preference(productions: List[ProductionBase], token: Annotated[TokenData,Depends(get_current_user)]):
     preferences = [LikeProduction(id_user=int(token.uid), id_production=production.uid).dict() for production in productions]
     result = requests.post(f"{PRODUCTION_URL}/like", data=json.dumps(preferences))
     result.raise_for_status()
     return result.json()
 
 @router.delete("/production")
-def delete_genre_preference(production: ProductionBase,token: Annotated[TokenData,Depends(get_current_user)]):
+def delete_production_preference(production: ProductionBase,token: Annotated[TokenData,Depends(get_current_user)]):
     preference= LikeProduction(id_user=int(token.uid),id_production=production.uid)
     result = requests.delete(f"{PRODUCTION_URL}/like",data=preference.json())
     result.raise_for_status()
     return result.json()
 
 @router.get("/staff")
-def get_genre_preference(token: Annotated[TokenData,Depends(get_current_user)]):
+def get_staff_preference(token: Annotated[TokenData,Depends(get_current_user)]):
     db_preferences = requests.get(f"{STAFF_URL}/like/all", params={"uid":token.uid})
     db_preferences.raise_for_status()
     return db_preferences.json()
 
 @router.post("/staff")
-def create_genre_preference(staffs: List[StaffBase], token: Annotated[TokenData,Depends(get_current_user)]):
+def create_staff_preference(staffs: List[StaffBase], token: Annotated[TokenData,Depends(get_current_user)]):
     preferences = [LikeStaff(id_user=int(token.uid), id_staff=staff.uid).dict() for staff in staffs]
     result = requests.post(f"{STAFF_URL}/like", data=json.dumps(preferences))
     result.raise_for_status()
     return result.json()
 
 @router.delete("/staff")
-def delete_genre_preference(staff: StaffBase,token: Annotated[TokenData,Depends(get_current_user)]):
+def delete_staff_preference(staff: StaffBase,token: Annotated[TokenData,Depends(get_current_user)]):
     preference= LikeStaff(id_user=int(token.uid),id_staff=staff.uid)
     result = requests.delete(f"{STAFF_URL}/like",data=preference.json())
     result.raise_for_status()
