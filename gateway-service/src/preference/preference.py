1d719e20423753a8897ae59935c6526d4e0cb544
Dilvy Anais <dilvyanais@cy-tech.fr>
2023-04-24 13:53:02 +0200

    add:  production-service to gateway

--- gateway-service/src/preference/preference.py
+++ gateway-service/src/preference/preference.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter,Depends
 from ..auth.controller import get_current_user
 from typing import Annotated
-from schema import TokenData, GenreBase, Preferences
+from schema import TokenData, GenreBase, Preferences, ProductionBase, LikeProduction
 from dotenv import load_dotenv
 from typing import List
 import os
@@ -10,6 +10,7 @@ import json
 load_dotenv()
 
 GENRE_URL = os.getenv("GENRE_URL")
+PRODUCTION_URL= os.getenv("PRODUCTION_URL")
 
 router = APIRouter(
     prefix="/like",
@@ -33,5 +34,25 @@ def create_genre_preference(genres: List[GenreBase], token: Annotated[TokenData,
 def delete_genre_preference(genre: GenreBase,token: Annotated[TokenData,Depends(get_current_user)]):
     preference= Preferences(id_user=int(token.uid),id_genre=genre.uid)
     result = requests.delete(f"{GENRE_URL}/like",data=preference.json())
+    result.raise_for_status()
+    return result.json()
+
+@router.get("/production")
+def get_genre_preference(token: Annotated[TokenData,Depends(get_current_user)]):
+    db_preferences = requests.get(f"{PRODUCTION_URL}/like/all", params={"uid":token.uid})
+    db_preferences.raise_for_status()
+    return db_preferences.json()
+
+@router.post("/production")
+def create_genre_preference(productions: List[ProductionBase], token: Annotated[TokenData,Depends(get_current_user)]):
+    preferences = [LikeProduction(id_user=int(token.uid), id_production=production.uid).dict() for production in productions]
+    result = requests.post(f"{PRODUCTION_URL}/like", data=json.dumps(preferences))
+    result.raise_for_status()
+    return result.json()
+
+@router.delete("/production")
+def delete_genre_preference(production: ProductionBase,token: Annotated[TokenData,Depends(get_current_user)]):
+    preference= LikeProduction(id_user=int(token.uid),id_production=production.uid)
+    result = requests.delete(f"{PRODUCTION_URL}/like",data=preference.json())
     result.raise_for_status()
     return result.json()
