36a6b1b227f977881b06f5bfb9d46dffd6dd6c5c
Dilvy Anais <dilvyanais@cy-tech.fr>
2023-04-26 09:49:00 +0200

    fix: merge visualize-service into movie-service

--- movie-service/src/main.py
+++ movie-service/src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI,Depends
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm  import Session, sessionmaker
-from schema import  ProductionBase, Production, Staff, StaffBase, Genre, GenreBase, MovieId, Movie
+from schema import  ProductionBase, Production, Staff, StaffBase, Genre, GenreBase, MovieId, Movie, Visualize
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 from typing import List
@@ -163,3 +163,26 @@ def delete_staff(movie:MovieId, db: Session= Depends(get_db)):
 def update_staff(movie:Movie, db:Session=Depends(get_db)):
     ctrl.update_staff(movie, db)
     db.close()
+
+@app.post("/watch")
+def create_visualize_relationship(relations: List[Visualize],db: Session= Depends(get_db)):
+    result = ctrl.create_visualize_relation(relations,db)
+    db.close()
+    return result
+
+@app.get("/watch/all")
+def get_movies_seen_by_user(uid:int, db:Session= Depends(get_db)):
+    result = ctrl.get_movies_seen_by_user(uid,db)
+    db.close()
+    return result
+
+@app.delete("/watch")
+def delete_visualize_relationship(relation:Visualize,db:Session=Depends(get_db)):
+    ctrl.delete_visualize_relation(relation,db)
+    db.close()
+
+@app.patch("/watch")
+def update_visualize_relationship(new_relation:Visualize,old_relation:Visualize,db:Session=Depends(get_db)):
+    ctrl.update_visualize_relation(new_relation=new_relation, old_relation=old_relation,db=db)
+    db.close()
+ 
