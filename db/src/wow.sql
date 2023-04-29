8d75b3e92c32a3e3aa40aef42cfb3eec4e6b0bf9
Dilvy Anais <dilvyanais@cy-tech.fr>
2023-04-30 00:14:21 +0200

    fix: wow-sql

--- db/src/wow.sql
+++ db/src/wow.sql
@@ -33,7 +33,7 @@ CREATE TABLE `auth` (
   `password` text NOT NULL,
   PRIMARY KEY (`uid`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
