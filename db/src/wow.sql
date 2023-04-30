2d8b889c43baf88f875a8de8225c7ddc46d5c358
Hiruurouge <Hiruurouge@gmail.com>
2023-04-30 14:26:43 +0200

    DB_LA_DB_WESH

--- db/src/wow.sql
+++ db/src/wow.sql
@@ -18,6 +18,9 @@
 --
 -- Table structure for table `auth`
 --
+SET GLOBAL connect_timeout=28800;
+SET GLOBAL interactive_timeout=28800;
+SET GLOBAL wait_timeout=28800;
 GRANT ALL PRIVILEGES ON * . * TO app;
 
 CREATE DATABASE IF NOT EXISTS wow;
